fad3b78ba927cede6d312ada900be1dc8c2ea0b8
Shaun <170037375+sharrisondev@users.noreply.github.com>
2025-07-16 15:08:34 +0100

    Added project folder and completed the Console Creature project

---
+++ Projects/ConsoleCreatures/ConsoleCreatures/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCreatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program prints an ASCII art creature to the console.
+            Console.WriteLine("     (    )");
+            Console.WriteLine("  (  (   )  )");
+            Console.WriteLine("(  (       )  )");
+            Console.WriteLine("  (  (   )    )");
+            Console.WriteLine("     (     )");
+            Console.WriteLine("   /--------\\");
+            Console.WriteLine("   /   \\   /  \\");
+            Console.WriteLine("  /     0 0    \\");
+            Console.WriteLine(" /       o      \\");
+            Console.WriteLine(" ****************");
+
+            Console.WriteLine("");
+            Console.WriteLine("This is Angry Volcano, the console creature!");
+        }
+    }
+}
