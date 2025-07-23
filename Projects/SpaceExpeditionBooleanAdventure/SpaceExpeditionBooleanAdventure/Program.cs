660f12088e62de49bbcf27e28b0b62592d4fba2f
Shaun <170037375+sharrisondev@users.noreply.github.com>
2025-07-23 18:08:49 +0100

    Adding the Space Expedition Boolean Adventure project

---
+++ Projects/SpaceExpeditionBooleanAdventure/SpaceExpeditionBooleanAdventure/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceExpeditionBooleanAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAtmosphereBreathable = true;
+            bool isGravityStable = false;
+            bool resourcesSufficient = true;
+            int currentCrew = 5;
+            int maxCrewCapacity = 10;
+
+            bool isHabitable = isAtmosphereBreathable && isGravityStable;
+            Console.WriteLine(isHabitable);
+
+            bool suitableForExpansion = isGravityStable || resourcesSufficient;
+            Console.WriteLine(suitableForExpansion);
+
+            bool canAcceptMoreCrew = currentCrew < maxCrewCapacity;
+            Console.WriteLine(canAcceptMoreCrew);
+        }
+    }
+}
